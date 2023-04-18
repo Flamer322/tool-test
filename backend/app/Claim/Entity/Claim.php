8578f88b6aa4e576c0316cf8cf392220ce2b2c6a
flamer322 <b.bar1enev@gmail.com>
2023-04-18 13:09:19 +0300

    Claim model

---
+++ backend/app/Claim/Entity/Claim.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Claim\Entity;
+
+use App\User\Entity\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+/**
+ * App\Claim\Entity\Claim
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $name
+ * @property string|null $number
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @property-read User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereUserId($value)
+ * @mixin \Eloquent
+ */
+class Claim extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    public const MEDIA_COLLECTION = 'claim.files';
+
+    protected $fillable = [
+        'name',
+        'number',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection(self::MEDIA_COLLECTION);
+    }
+}
